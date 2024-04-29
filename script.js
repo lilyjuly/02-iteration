6e10254ad205b32563ffa50521be983db80198f6
lily <lilyjulymm2009@gmail.com>
2024-04-29 20:44:19 +0630

    02-iteration finish

---
+++ script.js
@@ -0,0 +1,38 @@
+var country = ['Myanmar','India','Calbodia','Singapore','Japan'];
+console.log(country.length);
+for (var i=0; i<country.length; i++){
+    console.log("I want to go" +" " + country[i] + "!");
+};
+
+var c = 50;
+var d1 = c < 50;
+var d2 = c > 50;
+
+if (d1 || d2){
+    console.log("T");
+}else {
+    console.log("F");
+};
+
+
+var students = ['Mg Mg','gogo','emo','koko','CDM'];
+console.log(students.length);
+for (var i=0; i<students.length; i++){
+    console.log("My name is" +" "+ students[i] + "!");
+};
+
+// While Loop
+let n = 0;
+while(n < 11){
+    console.log(n);
+    n++
+}
+var letters = ["A ","5 ","M ","0 ","Z ","2 "];
+for(letters of letters){
+    console.log(letters);
+}
+var K = 0;
+while(K < 5){
+    console.log("This number is " + K);
+    K = K + 1
+}
